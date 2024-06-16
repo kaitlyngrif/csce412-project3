ad35de6c41f986c65416f54ab73b1e10c1ea4126
Kaitlyn Griffin <griffin.kaitlyn@tamu.edu>
2024-06-16 12:08:51 -0500

    tentative load balancer header made

--- LoadBalancer.h
+++ LoadBalancer.h
@@ -2,10 +2,36 @@
 #ifndef LOADBALANCER_H
 #define LOADBALANCER_H
 
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <string>
+#include "RequestQueue.h"
+#include "WebServer.h"
+
 class LoadBalancer {
     // variables here
+private:
+    RequestQueue requestQueue;
+    std::vector<WebServer*> webServers;
+    int time;
+    int serverCount;
 
     // functions here
+public:
+    LoadBalancer();
+    //LoadBalancer(int serverCount);
+    LoadBalancer(RequestQueue& requestQueue);
+    //LoadBalancer(RequestQueue requestQueue, std::vector<WebServer*> webServers);
+    ~LoadBalancer();
+    void runBalancer(int time, int serverCount);
+    void balanceLoad();
+    void addRequest(Request* request);
+    void addWebServer(WebServer* webServer);
+    void processRequest();
+    void print();
+    bool isEmpty();
+
 };
 
 #endif
